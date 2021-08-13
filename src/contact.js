52eea7e98c2a47c6a67d02955e128b50a1ba7af6
Dev-Sherlock <khanbayov.tamerlan@gmail.com>
2021-08-13 15:27:06 +0200

    added contact form.

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,70 @@
 import React from 'react';
+import axios from 'axios';
 
-function Contact(props) {
-    return <h2>Contact</h2>;
+class Contact extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      full_name: '',
+      email: '',
+      message: ''
+    }
   }
+
+  handleSubmit(e){
+    e.preventDefault();
+    axios({
+      method: "POST",
+      url:"http://localhost:3002/send",
+      data:  this.state
+    }).then((response)=>{
+      if (response.data.status === 'success') {
+        alert("Message was sent successfully.");
+        this.resetForm()
+      } else if (response.data.status === 'fail') {
+        alert("Message failed to send.")
+      }
+    })
+  }
+
+  resetForm(){
+    this.setState({full_name: '', email: '', message: ''})
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+          <div className="form-group">
+              <label htmlFor="name">Full name</label>
+              <input type="text" className="form-control" id="name" value={this.state.name} onChange={this.onNameChange.bind(this)} />
+          </div>
+          <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+          </div>
+          <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <textarea className="form-control" rows="5" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    );
+  }
+
+  onNameChange(event) {
+	  this.setState({full_name: event.target.value})
+  }
+
+  onEmailChange(event) {
+	  this.setState({email: event.target.value})
+  }
+
+  onMessageChange(event) {
+	  this.setState({message: event.target.value})
+  }
+}
 
 export default Contact;
